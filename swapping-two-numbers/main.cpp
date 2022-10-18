927caee9bf2a393f59d4132dfb239b83dc1f14d3
nakayan5 <tomoya.progress@gmail.com>
2022-10-19 02:33:46 +0900

    add: swapping-two-numbers

---
+++ swapping-two-numbers/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x, y;
+
+    for (int i=1;;i++){
+        cin >> x >> y;
+        if (x == 0 && y == 0)break;
+        if (x <= y){
+          cout << x << " " << y << endl;
+        } else {
+          cout << y << " " << x << endl;
+        }
+    }
+    
+    return 0;
+}
