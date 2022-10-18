3b96c5aab45afa4453b564faee35234f259ee10b
nakayan5 <tomoya.progress@gmail.com>
2022-10-18 22:29:58 +0900

    add: print-many-hello-world

---
+++ print-many-hello-world/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 1;
+    
+    do {
+        cout << "Hello World" << endl;
+        count++;
+    } while (count <= 1000);
+    
+    return 0;
+}
