369123f959d362066aae5c2012451bcd5c1c9c84
nakayan5 <tomoya.progress@gmail.com>
2022-10-19 17:03:45 +0900

    add: print-a-rectangle

---
+++ print-a-rectangle/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int H, W;
+    
+    while (1){
+        cin >> H >> W;
+        if (H == 0 && W == 0) break;
+        
+        for (int h = 1; h <= H; h++){
+            for (int w = 1; w <= W; w++){
+                cout << '#';
+            }
+            cout << endl;
+        }
+        
+    cout << endl;
+  }
+  
+  return 0;
+}
